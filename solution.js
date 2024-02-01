22076193e62798c3322a07d0777b87fdd39eefed
ChrisNieves <nievescs20@gmail.com>
2024-01-31 23:58:53 -0500

    added test cases

--- solution.js
+++ solution.js
@@ -1,3 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
 function removeElements(head, val) {
   if (head === null) return head;
   let previous = new ListNode(null, null);
@@ -14,3 +29,25 @@ function removeElements(head, val) {
   }
   return head.next;
 }
+
+const test1 = new ListNode(
+  1,
+  new ListNode(
+    2,
+    new ListNode(
+      6,
+      new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, null))))
+    )
+  )
+);
+
+const test2 = null;
+
+const test3 = new ListNode(
+  7,
+  new ListNode(7, new ListNode(7, new ListNode(7, null)))
+);
+
+console.log(removeElements(test1, 6));
+console.log(removeElements(test2, 1));
+console.log(removeElements(test3, 7));
